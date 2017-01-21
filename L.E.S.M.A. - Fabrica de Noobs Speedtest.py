3c132e56d38bd546cf5b02d39859bb19a58958c8
NatanSkywalker <fabricadenoobs2@gmail.com>
2017-01-20 22:08:09 -0200

    Add files via upload

--- L.E.S.M.A. - Fabrica de Noobs Speedtest.py
+++ L.E.S.M.A. - Fabrica de Noobs Speedtest.py
@@ -1,3 +1,21 @@
+#Programado por: Natanael Antonioli - Fábrica de Noobs
+#Baseado no código de Matt Martz
+
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+#Morte ao miojo!
+
+
 a= 0
 import time
 from time import gmtime, strftime
@@ -6,6 +24,8 @@ import datetime
 
  
 print (" ")
+print ("Relatório obtido com através do L.E.S.M.A")
+print ("Site oficial: https://github.com/NatanSkywalker/L.E.S.M.A")
 print (" ")
 num = 0
 
@@ -17,22 +37,8 @@ while a == 0:
     print (" ")
 
     
-    #!/usr/bin/env python
-    # -*- coding: utf-8 -*-
-    # Copyright 2012-2016 Matt Martz
-    # All Rights Reserved.
-    #
-    #    Licensed under the Apache License, Version 2.0 (the "License"); you may
-    #    not use this file except in compliance with the License. You may obtain
-    #    a copy of the License at
-    #
-    #         http://www.apache.org/licenses/LICENSE-2.0
-    #
-    #    Unless required by applicable law or agreed to in writing, software
-    #    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-    #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-    #    License for the specific language governing permissions and limitations
-    #    under the License.
+    
+
 
     import os
     import re
